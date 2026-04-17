5139fd559f2a32201f8a84483b7b5c8f11a1d773
Abdullah Shah <syedabdullahshah086@gmail.com>
2026-04-17 19:18:51 +0500

    MedicalStaff completed

--- src/model/MedicalStaff.java
+++ src/model/MedicalStaff.java
@@ -15,6 +15,39 @@ public abstract class MedicalStaff extends Person {
         this.salary = salary;
         this.shiftSchedule = shiftSchedule;
     }
+    //gettters and setters
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getShiftSchedule() {
+        return shiftSchedule;
+    }
+
+    public void setShiftSchedule(String shiftSchedule) {
+        this.shiftSchedule = shiftSchedule;
+    }
 
     public abstract void treat(Patient patient);
 }
