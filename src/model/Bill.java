5a18e8157eb33eac4e13af082ef102521f3434ea
Abdullah Shah <syedabdullahshah086@gmail.com>
2026-04-17 18:28:57 +0500

    bill completed

--- src/model/Bill.java
+++ src/model/Bill.java
@@ -11,13 +11,30 @@ public class Bill implements Billable {
     private boolean isPaid;
 
     public Bill(String billId, double roomCharges, double medicineCharges,
-                double consultationFee) {}
+                double consultationFee) {
+        this.billId = billId;
+        this.roomCharges = roomCharges;
+        this.medicineCharges = medicineCharges;
+        this.consultationFee = consultationFee;
+        this.isPaid = false;
+    }
 
     public double calculateBill() {
         return roomCharges + medicineCharges + consultationFee;
     }
 
-    public void generateInvoice() {}
+    public void generateInvoice() {
+        System.out.println("===== Bill Reciept =====");
+        System.out.println("------------------------");
+        System.out.println("  Bill ID : "+billId);
+        System.out.println("  Room Charges : "+ roomCharges);
+        System.out.println("  Medicine Charges : "+medicineCharges);
+        System.out.println("  Consultaion Fee : "+consultationFee);
+        System.out.println("  Total Bill : "+calculateBill());
+        //System.out.println("  Status : "+ isPaid);
+    }
 
-    public void markAsPaid() {}
+    public void markAsPaid() {
+        this.isPaid = true;
+    }
 }
