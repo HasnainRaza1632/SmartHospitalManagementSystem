a51a9a45a46d8c36deb4baa8a2dfc3d56af7fa47
Hasnain Raza <chandiohasnain807@gmail.com>
2026-04-17 18:10:28 +0400

    ICU room completed

--- src/model/ICURoom.java
+++ src/model/ICURoom.java
@@ -8,12 +8,33 @@ public class ICURoom extends Room {
 
     public ICURoom(String roomNumber, String roomType, boolean isOccupied, double dailyCharge,
                    boolean ventilatorAvailable, boolean monitorAvailable, double criticalCharge) {
-        super(roomNumber, roomType, isOccupied, dailyCharge);
+        super(roomNumber, roomType, dailyCharge);
+        this.criticalCharge = criticalCharge;
+        this.monitorAvailable = monitorAvailable;
+        this.ventilatorAvailable = ventilatorAvailable;
     }
 
-    public void allocate() {}
+    @Override
+    public void allocate() {
+        if(!isOccupied){
+            isOccupied = true;
+            System.out.println("Emergency Room " + getRoomNumber() + " allocated successfully.");
+            return;
+        }
+        System.out.println("Emergency Room " + getRoomNumber() + " is already occupied");
+    }
 
-    public void triggerEmergencyAlert() {}
+    public void triggerEmergencyAlert() {
+        System.out.println("Emergency alert in ICU room " + getRoomNumber());
+    }
 
-    public void vacate() {}
+    public void vacate() {
+        if(isOccupied){
+            isOccupied = false;
+            System.out.println("Room "+ getRoomNumber() + " has been vacated.");
+            return;
+        }
+        System.out.println("Room " + getRoomNumber() + " is already empty.");
+    }
+
 }
