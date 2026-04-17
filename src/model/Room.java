e1465b0aa99a085971c36defe13a038a3ae20132
Hasnain Raza <chandiohasnain807@gmail.com>
2026-04-17 17:53:02 +0400

    Room completed

--- src/model/Room.java
+++ src/model/Room.java
@@ -7,16 +7,35 @@ public class Room {
     private boolean isOccupied;
     private double dailyCharge;
 
-    public Room(String roomNumber, String roomType, boolean isOccupied, double dailyCharge) {
+    public Room(String roomNumber, String roomType, double dailyCharge) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
-        this.isOccupied = isOccupied;
+        this.isOccupied = false;
         this.dailyCharge = dailyCharge;
     }
 
     public String getRoomNumber(){return this.roomNumber;}
+    public String getRoomType(){return this.roomType;}
+    public boolean isOccupied(){return this.isOccupied;}
+    public double getDailyCharge(){return this.dailyCharge;}
 
-    public void allocate() {}
+    public void setDailyCharge(double dailyCharge){this.dailyCharge = dailyCharge;}
 
-    public void vacate() {}
+    public void allocate() {
+        if(!isOccupied){
+            isOccupied = true;
+            System.out.println("Room " + roomNumber + " allocated successfully.");
+            return;
+        }
+        System.out.println("Room " + roomNumber + " is already occupied");
+    }
+
+    public void vacate() {
+        if(isOccupied){
+            isOccupied = false;
+            System.out.println("Room "+ roomNumber + " has been vacated.");
+            return;
+        }
+        System.out.println("Room " + roomNumber + " is already empty.");
+    }
 }
